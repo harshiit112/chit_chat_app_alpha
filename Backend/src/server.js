97c14a183b78a5d9e95332f6144876f94d42406c
harshitit112 <hv702383@gmail.com>
2025-06-26 01:53:06 +0530

    feat: implement authentication routes and database connection

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,25 +1,16 @@
 import express from "express"
+import "dotenv/config"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT
 
-app.get("/api/auth/signup", (req, res) => {
-    res.send("Singup Route");
-});
-
-app.get("/api/auth/login", (req, res) => {
-    res.send("Login Route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-    res.send("Logout Route");
-});
+app.use("/api/auth", authRoutes)
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
